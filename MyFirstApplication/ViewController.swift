15ad6c211576b2d3ff6e98f9269902f728877728
michaelroscero <michaelceronio2005@gmail.com>
2025-01-26 21:28:37 -0600

    Initial Commit

---
+++ MyFirstApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApplication
+//
+//  Created by Michael Rosas Ceronio on 1/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
